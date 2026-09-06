d23bd6f4835029c2f20379af5bd5068223593647
Aiden Fong <afong413@gmail.com>
2026-09-06 14:57:07 -0700

    Add tqdm bar to lines input

--- src/aoc/input.py
+++ src/aoc/input.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from typing import Literal, overload
+
+from tqdm.auto import tqdm
 
 import __main__
 
@@ -8,5 +11,14 @@ def input() -> str:
     return path.read_text().strip()
 
 
-def lines() -> list[str]:
-    return input().split('\n')
+@overload
+def lines(bar: Literal[False]) -> list[str]: ...
+@overload
+def lines(bar: Literal[True]) -> tqdm[str]: ...
+@overload
+def lines() -> tqdm[str]: ...
+def lines(bar: bool = True) -> list[str] | tqdm[str]:
+    l = input().split('\n')
+    if bar:
+        return tqdm(input().split('\n'))
+    return l
