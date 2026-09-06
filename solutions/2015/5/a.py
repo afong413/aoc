84cd35936dd16c08203f9828296cf69321617978
Aiden Fong <afong413@gmail.com>
2026-09-06 14:33:50 -0700

    2015 Day 5

---
+++ solutions/2015/5/a.py
@@ -0,0 +1,22 @@
+import aoc
+
+ans = 0
+
+for word in aoc.lines():
+    if not word.strip():
+        continue
+    word += ' '
+    v = 0
+    d = False
+    for i in range(len(word) - 1):
+        if word[i] in 'aeiou':
+            v += 1
+        if word[i] == word[i + 1]:
+            d = True
+        if word[i : i + 2] in ['ab', 'cd', 'pq', 'xy']:
+            break
+    else:
+        if v >= 3 and d:
+            ans += 1
+
+print(ans)
---
+++ solutions/2015/5/b.py
@@ -0,0 +1,21 @@
+import aoc
+
+ans = 0
+
+for word in aoc.lines():
+    if not word.strip():
+        continue
+    word += ' '
+    xyx = False
+    double = False
+    for i in range(len(word) - 2):
+        if word[i] == word[i + 2]:
+            xyx = True
+        for j in range(i + 2, len(word) - 2):
+            if word[i : i + 2] == word[j : j + 2]:
+                double = True
+        if xyx and double:
+            ans += 1
+            break
+
+print(ans)
